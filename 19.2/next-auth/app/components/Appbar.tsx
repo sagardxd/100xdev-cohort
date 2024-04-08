5f8d4815afebb8c1205ee597609146c4f0ac239f
Sagar Yadav <sagardxd5@gmail.com>
2024-04-08 17:32:30 +0530

    signin/nextauth

--- 19.2/next-auth/app/components/Appbar.tsx
+++ 19.2/next-auth/app/components/Appbar.tsx
@@ -1,12 +1,17 @@
 "use client";
 
 import { useRouter } from "next/navigation"
+import{ signIn, signOut} from "next-auth/react"
 
 export const Appbar = () => {
     const router = useRouter();
     return <div>
         <button onClick={() => {
-            router.push("/api/auth/signin")
+            signIn();
         }}>Login</button>
+
+<button onClick={() => {
+            signOut()
+        }}>Logout</button>
     </div>
 }
