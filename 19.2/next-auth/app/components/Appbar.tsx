e26058fca4175461664eb54f1bdb09da1481c153
Sagar Yadav <sagardxd5@gmail.com>
2024-04-08 17:59:00 +0530

    session id

--- 19.2/next-auth/app/components/Appbar.tsx
+++ 19.2/next-auth/app/components/Appbar.tsx
@@ -1,10 +1,12 @@
 "use client";
 
 import { useRouter } from "next/navigation"
-import{ signIn, signOut} from "next-auth/react"
+import{ signIn, signOut, useSession} from "next-auth/react"
 
 export const Appbar = () => {
     const router = useRouter();
+    const session = useSession();
+
     return <div>
         <button onClick={() => {
             signIn();
@@ -13,5 +15,6 @@ export const Appbar = () => {
 <button onClick={() => {
             signOut()
         }}>Logout</button>
+        {JSON.stringify(session)}
     </div>
 }
