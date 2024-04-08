cc39a2c54d83ec79ad5deec0e4297bed76c85715
Sagar Yadav <sagardxd5@gmail.com>
2024-04-08 17:14:19 +0530

    next-auth

---
+++ 19.2/next-auth/app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,26 @@
+import { RequestInternal, Awaitable, User } from "next-auth";
+import NextAuth from "next-auth/next";
+import CredentialsProvider from "next-auth/providers/credentials";
+
+const handler = NextAuth({
+    providers: [
+        CredentialsProvider({
+            name: "Email",
+            credentials: {
+                username: { label: "Username", type: "text", placeholder: "sagardxd" },
+                password: { label: "Password", type: "password", placeholder:"123456" }
+            },
+            async authorize(credentials: any) {
+
+                console.log(credentials``)
+                return {
+                    id: "user1"
+                }   
+            }
+        })
+
+    ]
+})
+
+export const GET = handler;
+export const POST = handler;
